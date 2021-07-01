910678c322f829ba8e7b4f9627458e275112dd41
Berzaf <70249457+Berzaf@users.noreply.github.com>
2021-06-30 20:07:16 -0500

    create currency calculator

---
+++ ExchangeRateCalculator/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace ExchangeRateCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.amountLabel = new System.Windows.Forms.Label();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.fromGroupBox = new System.Windows.Forms.GroupBox();
+            this.fromGbpRadioButton = new System.Windows.Forms.RadioButton();
+            this.fromEurRadioButton = new System.Windows.Forms.RadioButton();
+            this.fromUsdRadioButton = new System.Windows.Forms.RadioButton();
+            this.exchangeTextBox = new System.Windows.Forms.TextBox();
+            this.exchangeLabel = new System.Windows.Forms.Label();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.toGroupBox = new System.Windows.Forms.GroupBox();
+            this.toGbpRadioButton = new System.Windows.Forms.RadioButton();
+            this.toEurRadioButton = new System.Windows.Forms.RadioButton();
+            this.toUsdRadioButton = new System.Windows.Forms.RadioButton();
+            this.fromGroupBox.SuspendLayout();
+            this.toGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // amountLabel
+            // 
+            this.amountLabel.AutoSize = true;
+            this.amountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amountLabel.Location = new System.Drawing.Point(200, 69);
+            this.amountLabel.Name = "amountLabel";
+            this.amountLabel.Size = new System.Drawing.Size(103, 16);
+            this.amountLabel.TabIndex = 0;
+            this.amountLabel.Text = "Enter Amount:";
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(343, 66);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.amountTextBox.TabIndex = 1;
+            // 
+            // fromGroupBox
+            // 
+            this.fromGroupBox.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.fromGroupBox.Controls.Add(this.fromGbpRadioButton);
+            this.fromGroupBox.Controls.Add(this.fromEurRadioButton);
+            this.fromGroupBox.Controls.Add(this.fromUsdRadioButton);
+            this.fromGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fromGroupBox.Location = new System.Drawing.Point(62, 149);
+            this.fromGroupBox.Name = "fromGroupBox";
+            this.fromGroupBox.Size = new System.Drawing.Size(134, 118);
+            this.fromGroupBox.TabIndex = 2;
+            this.fromGroupBox.TabStop = false;
+            this.fromGroupBox.Text = "From";
+            // 
+            // fromGbpRadioButton
+            // 
+            this.fromGbpRadioButton.AutoSize = true;
+            this.fromGbpRadioButton.Location = new System.Drawing.Point(43, 95);
+            this.fromGbpRadioButton.Name = "fromGbpRadioButton";
+            this.fromGbpRadioButton.Size = new System.Drawing.Size(57, 20);
+            this.fromGbpRadioButton.TabIndex = 2;
+            this.fromGbpRadioButton.TabStop = true;
+            this.fromGbpRadioButton.Text = "GBP";
+            this.fromGbpRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // fromEurRadioButton
+            // 
+            this.fromEurRadioButton.AutoSize = true;
+            this.fromEurRadioButton.Location = new System.Drawing.Point(43, 59);
+            this.fromEurRadioButton.Name = "fromEurRadioButton";
+            this.fromEurRadioButton.Size = new System.Drawing.Size(58, 20);
+            this.fromEurRadioButton.TabIndex = 1;
+            this.fromEurRadioButton.TabStop = true;
+            this.fromEurRadioButton.Text = "EUR";
+            this.fromEurRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // fromUsdRadioButton
+            // 
+            this.fromUsdRadioButton.AutoSize = true;
+            this.fromUsdRadioButton.Location = new System.Drawing.Point(42, 19);
+            this.fromUsdRadioButton.Name = "fromUsdRadioButton";
+            this.fromUsdRadioButton.Size = new System.Drawing.Size(58, 20);
+            this.fromUsdRadioButton.TabIndex = 0;
+            this.fromUsdRadioButton.TabStop = true;
+            this.fromUsdRadioButton.Text = "USD";
+            this.fromUsdRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // exchangeTextBox
+            // 
+            this.exchangeTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.exchangeTextBox.Location = new System.Drawing.Point(575, 191);
+            this.exchangeTextBox.Name = "exchangeTextBox";
+            this.exchangeTextBox.Size = new System.Drawing.Size(121, 20);
+            this.exchangeTextBox.TabIndex = 4;
+            // 
+            // exchangeLabel
+            // 
+            this.exchangeLabel.AutoSize = true;
+            this.exchangeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exchangeLabel.Location = new System.Drawing.Point(438, 191);
+            this.exchangeLabel.Name = "exchangeLabel";
+            this.exchangeLabel.Size = new System.Drawing.Size(116, 16);
+            this.exchangeLabel.TabIndex = 3;
+            this.exchangeLabel.Text = "Your Exchange:";
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConvertButton.Location = new System.Drawing.Point(62, 337);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(75, 50);
+            this.ConvertButton.TabIndex = 5;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(273, 337);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 50);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(456, 337);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 50);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // toGroupBox
+            // 
+            this.toGroupBox.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.toGroupBox.Controls.Add(this.toGbpRadioButton);
+            this.toGroupBox.Controls.Add(this.toEurRadioButton);
+            this.toGroupBox.Controls.Add(this.toUsdRadioButton);
+            this.toGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toGroupBox.Location = new System.Drawing.Point(298, 149);
+            this.toGroupBox.Name = "toGroupBox";
+            this.toGroupBox.Size = new System.Drawing.Size(134, 118);
+            this.toGroupBox.TabIndex = 8;
+            this.toGroupBox.TabStop = false;
+            this.toGroupBox.Text = "To";
+            // 
+            // toGbpRadioButton
+            // 
+            this.toGbpRadioButton.AutoSize = true;
+            this.toGbpRadioButton.Location = new System.Drawing.Point(43, 95);
+            this.toGbpRadioButton.Name = "toGbpRadioButton";
+            this.toGbpRadioButton.Size = new System.Drawing.Size(57, 20);
+            this.toGbpRadioButton.TabIndex = 2;
+            this.toGbpRadioButton.TabStop = true;
+            this.toGbpRadioButton.Text = "GBP";
+            this.toGbpRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // toEurRadioButton
+            // 
+            this.toEurRadioButton.AutoSize = true;
+            this.toEurRadioButton.Location = new System.Drawing.Point(43, 59);
+            this.toEurRadioButton.Name = "toEurRadioButton";
+            this.toEurRadioButton.Size = new System.Drawing.Size(58, 20);
+            this.toEurRadioButton.TabIndex = 1;
+            this.toEurRadioButton.TabStop = true;
+            this.toEurRadioButton.Text = "EUR";
+            this.toEurRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // toUsdRadioButton
+            // 
+            this.toUsdRadioButton.AutoSize = true;
+            this.toUsdRadioButton.Location = new System.Drawing.Point(42, 19);
+            this.toUsdRadioButton.Name = "toUsdRadioButton";
+            this.toUsdRadioButton.Size = new System.Drawing.Size(58, 20);
+            this.toUsdRadioButton.TabIndex = 0;
+            this.toUsdRadioButton.TabStop = true;
+            this.toUsdRadioButton.Text = "USD";
+            this.toUsdRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toGroupBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.exchangeTextBox);
+            this.Controls.Add(this.exchangeLabel);
+            this.Controls.Add(this.fromGroupBox);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.amountLabel);
+            this.Name = "Form1";
+            this.Text = "Exchange Rate Calculator";
+            this.fromGroupBox.ResumeLayout(false);
+            this.fromGroupBox.PerformLayout();
+            this.toGroupBox.ResumeLayout(false);
+            this.toGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label amountLabel;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.GroupBox fromGroupBox;
+        private System.Windows.Forms.RadioButton fromGbpRadioButton;
+        private System.Windows.Forms.RadioButton fromEurRadioButton;
+        private System.Windows.Forms.RadioButton fromUsdRadioButton;
+        private System.Windows.Forms.TextBox exchangeTextBox;
+        private System.Windows.Forms.Label exchangeLabel;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.GroupBox toGroupBox;
+        private System.Windows.Forms.RadioButton toGbpRadioButton;
+        private System.Windows.Forms.RadioButton toEurRadioButton;
+        private System.Windows.Forms.RadioButton toUsdRadioButton;
+    }
+}
+
---
+++ ExchangeRateCalculator/Form1.cs
@@ -0,0 +1,160 @@
+﻿/*
+ * Author: Berzaf Teklu
+ * Programe Name: Exchange Rate Calculator
+ * Date: 10/25/20
+ * 
+ * Create an application that the user know how many pounds he/she will have when
+ * they get to England based on the number of dollars they have now and also do the 
+ * reverse. calculate how many dollars they will have when in England. Do the same
+ * for travelers going to Europe.
+ * 
+ * We need to calculate Exchange Rate of US Dolar, England Pound and Europe EURO:
+ *   - From USD to EURO and USD to GBP
+ *   - From EURO to USD and EURO to GBP
+ *   - From GBP to USD and GBP to EURO
+ * We have the input variables the amount in USD, EURO and GBP. 
+ * We also have the daily convation rate.
+ * 
+ * Finaly to calculate the exchange rate we have to read the convartion rate values from text file.
+ * Then the convertion rate :
+ *    USDtoEUR = Amount * ConvertionRate(USDtoEUR)     
+ *    USDtoEUR = Amount * ConvertionRate(USDtoGBP)     
+ *    EURtoUSD = Amount * ConvertionRate(EURtoUSD)     
+ *    EURtoGBP = Amount * ConvertionRate(EURtoGBP)     
+ *    GBPtoUSD = Amount * ConvertionRate(GBPtoUSD)     
+ *    GBPtoEUR = Amount * ConvertionRate(GBPtoEUR)     
+ * 
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ExchangeRateCalculator
+{
+    public partial class Form1 : Form
+    {
+        decimal amount;
+        decimal usdToGbp;
+        decimal usdToEur;
+        decimal eurToUsd;
+        decimal eurToGbp;
+        decimal gepToUsd;
+        decimal gepToEur;
+        decimal usdToEurCon;
+        decimal usdToGbpCon;
+        decimal eurToUsdCon;
+        decimal eurToGbpCon;
+        decimal gepToUsdCon;
+        decimal gepToEurCon;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+          
+
+        
+
+            if (decimal.TryParse(amountTextBox.Text, out amount) && amount >= 0)
+            {
+                try
+                {
+                    // A StreamReader variable 
+                    StreamReader inputFile;
+
+                    // Open the file and get a StreamReader object.
+                    inputFile = File.OpenText("ConvertionRate.txt");
+
+                    // Read the convertion rate values from the file
+                    usdToEurCon = decimal.Parse(inputFile.ReadLine());
+                    usdToGbpCon = decimal.Parse(inputFile.ReadLine());
+                    eurToUsdCon = decimal.Parse(inputFile.ReadLine());
+                    eurToGbpCon = decimal.Parse(inputFile.ReadLine());
+                    gepToUsdCon = decimal.Parse(inputFile.ReadLine());
+                    gepToEurCon = decimal.Parse(inputFile.ReadLine());
+
+                    if (fromUsdRadioButton.Checked && toEurRadioButton.Checked)
+                    {
+                        usdToEur = decimal.Parse(amountTextBox.Text) * usdToEurCon;
+
+                        exchangeTextBox.Text = usdToEur.ToString("n3");
+                    }
+                    else if (fromUsdRadioButton.Checked && toGbpRadioButton.Checked)
+                    {
+                        usdToGbp = decimal.Parse(amountTextBox.Text) * usdToGbpCon;
+
+                        exchangeTextBox.Text = usdToGbp.ToString("n3");
+
+
+                    }
+                    else if (fromEurRadioButton.Checked && toUsdRadioButton.Checked)
+                    {
+                        eurToUsd = decimal.Parse(amountTextBox.Text) * eurToUsdCon;
+
+                        exchangeTextBox.Text = eurToUsd.ToString("n3");
+                    }
+                    else if (fromEurRadioButton.Checked && toGbpRadioButton.Checked)
+                    {
+                        eurToGbp = decimal.Parse(amountTextBox.Text) * eurToGbpCon;
+
+                        exchangeTextBox.Text = eurToGbp.ToString("n3");
+                    }
+
+                    else if (fromGbpRadioButton.Checked && toUsdRadioButton.Checked)
+                    {
+                        gepToUsd = decimal.Parse(amountTextBox.Text) * decimal.Parse(inputFile.ReadLine());
+                        exchangeTextBox.Text = gepToUsd.ToString("n3");
+                    }
+                    else if (fromGbpRadioButton.Checked && toEurRadioButton.Checked)
+                    {
+                        gepToEur = decimal.Parse(amountTextBox.Text) * decimal.Parse(inputFile.ReadLine());
+                        exchangeTextBox.Text = gepToEur.ToString("n3");
+                    }
+
+                    // close the file
+                    inputFile.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numbers", "Invalid Input");
+
+            }
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // clear all
+            amountTextBox.Text = "";
+            exchangeTextBox.Text = "";
+            amountTextBox.Focus();
+
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form
+            this.Close();
+        }
+    }
+}
